0d61349ad34494a90ab473ae5bf86cba74863051
César Bitarães <caabitaraes@gmail.com>
2023-03-16 23:31:51 -0300

    Primeira aula de Fundamentos da OO

---
+++ Fundamentos_da_OO/FundamentosOO/Payments/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Payments;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Todo objeto (customer) é um tipo de referência. Armazena apenas o endereço. Fica armazenado na memória heap.
+        // Tipos de valor são armazenados na memória stack, que é mais rápida.
+        var customer = new Customer(); 
+    }
+    
+}
+
+class Customer
+{
+    public string Name { get; set; }
+}
+
+class Payment
+{
+    // Encapsulamento (primeiro pilar): agrupar o que faz sentido estar junto. Nesse caso é criar tudo o que é de pagamento dentro da sua classe.
+    // Abstração (segundo pilar): esconder tudo aquilo que a gente não precisa saber sobre o nosso objeto. Métodos privados são um exemplo.
+    
+    // Propriedades
+    public DateTime Vencimento { get; set; }
+
+    // Métodos
+    void Pagar()
+    {
+        ConsultarSaldoDoCartao();
+
+    }
+
+    private void ConsultarSaldoDoCartao()
+    {
+        
+    }
+}
