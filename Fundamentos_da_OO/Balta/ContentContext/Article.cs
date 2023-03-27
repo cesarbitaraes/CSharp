d96e7cfa4952567000f4f8f822bb01c930adc64c
César Bitarães <caabitaraes@gmail.com>
2023-03-27 07:39:46 -0300

    Adicionando o curso da parte prática de OO

---
+++ Fundamentos_da_OO/Balta/ContentContext/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Balta.ContentContext;
+
+public class Article : Content
+{
+    public Article(string title, string url)
+        : base(title, url)
+    {
+        
+    }
+}
---
+++ Fundamentos_da_OO/Balta/Program.cs
@@ -0,0 +1,21 @@
+﻿using Balta.ContentContext;
+
+namespace Balta;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var articles = new List<Article>();
+        articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+        articles.Add(new Article("Artigo sobre C#", "csharp"));
+        articles.Add(new Article("Artigo sobre .Net", "dotnet"));
+
+        foreach (var article in articles)
+        {
+            Console.WriteLine(article.Id);
+            Console.WriteLine(article.Title);
+            Console.WriteLine(article.Url);
+        }
+    }
+}
